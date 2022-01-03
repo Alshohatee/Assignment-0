a2b2558bfd4934f6f341f82382814050e6200345
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 02:35:54 -0500

    slove productOfAnyAmountOfNumbers

--- 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
+++ 16-productOfAnyAmountOfNumbers/productOfAnyAmountOfNumbers.js
@@ -1,6 +1,12 @@
 function productOfAnyAmountOfNumbers(...args) {
-  // Insert code here;
-}
+    // Insert code here;
+    let product = 1;
 
-// Do not edit this line;
+    for (let i of args)
+        product *= i;
+
+    return product;
+}
+console.log(productOfAnyAmountOfNumbers(3, 4, 5))
+    // Do not edit this line;
 module.exports = productOfAnyAmountOfNumbers;
