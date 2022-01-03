07ac57cf6c8c40f3a0e6a4040e043155bb871578
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 01:38:25 -0500

    slove fizzBuzz

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,22 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+    // Insert code here;
+    let holder = [];
+
+    for (var i = start; i <= end; i++) {
+        if (i % 3 == 0 && i % 5 != 0)
+            holder.push("Fizz");
+
+        else if (i % 3 != 0 && i % 5 == 0)
+            holder.push("Buzz");
+
+        else if (i % 3 == 0 && i % 5 == 0)
+            holder.push("FizzBuzz");
+        else { //otherwise
+            holder.push(i);
+        }
+    }
+
+    return holder;
 }
 
 // Do not edit this line;
