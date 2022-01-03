97d191fd5a8e742df726b0339af5f00471f3f4f4
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 02:13:47 -0500

    slove frequencyCounter

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,6 +1,18 @@
 function frequencyCounter(word) {
-  // Insert code here;
-}
+    // Insert code here;
+    let frequency = {};
+    let char;
+    for (let i = 0; i < word.length; i++) {
+        char = word.charAt(i)
+        console.log(frequency[char]);
+        if (frequency[char])
+            frequency[char] = frequency[char] + 1
+        else
+            frequency[char] = 1
+    }
 
+    return frequency;
+}
+console.log(frequencyCounter("Aseell"));
 // Do not edit this line;
 module.exports = frequencyCounter;
