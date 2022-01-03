e6329e577cf4573ee38db9766136c71ae52a93de
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 00:40:20 -0500

    solve subtractFive

--- 02-subtractFive/subtractFive.js
+++ 02-subtractFive/subtractFive.js
@@ -1,6 +1,8 @@
 function subtractFive(num) {
-  // Insert code here;
+    // Insert code here;
+    return num - 5;
 }
 
+// console.log(subtractFive(10))
 // Do not edit this line;
 module.exports = subtractFive;
