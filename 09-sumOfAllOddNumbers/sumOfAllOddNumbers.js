dd44b84c271ccc7bb7b81ef125a0354f3ab83926
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 01:21:43 -0500

    slove sumOfAllOddNumbers

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,6 +1,13 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+    // Insert code here;
+    let counter = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 != 0)
+            counter++
+    }
+    return counter
 }
-
-// Do not edit this line;
+console.log(
+        sumOfAllOddNumbers([2, 4, 6, 7, 8, 9, 10]))
+    // Do not edit this line;
 module.exports = sumOfAllOddNumbers;
