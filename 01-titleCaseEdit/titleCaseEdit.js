82e8fb58f973d9c0c375798851a17e045f093ef2
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 00:30:23 -0500

    slove titleCaseEdit

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,20 @@
 function titleCaseEdit(title) {
-  // Insert code here;
-}
+    // Insert code here;
 
-// Do not edit this line;
+    // convert the title string to lower
+    // case and store it in array of string
+    let titleToArr = title.toLowerCase().split(' ');
+
+    for (let i = 0; i < titleToArr.length; i++) {
+      // convert the first letter to upper case of each word
+      titleToArr[i] = titleToArr[i].charAt(0).toUpperCase() + titleToArr[i].substring(1);
+     
+    }
+    // convert title back to string from an array
+    let titleBackToString = titleToArr.join(' ')  
+
+    return titleBackToString
+}
+console.log(titleCaseEdit("i am abdo"))
+    // Do not edit this line;
 module.exports = titleCaseEdit;
