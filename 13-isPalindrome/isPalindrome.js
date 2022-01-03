53298a3d709536ed2a49bf6ac0c1f509c0020c84
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 01:45:37 -0500

    slove isPalindrome

--- 13-isPalindrome/isPalindrome.js
+++ 13-isPalindrome/isPalindrome.js
@@ -1,6 +1,12 @@
 function isPalindrome(word) {
-  // Insert code here;
-}
+    // Insert code here
+    for (let i = 0; i < word.length; i++) {
+        if (word[i].toLowerCase() != word[word.length - 1 - i].toLowerCase())
+            return false
 
+    }
+
+    return true;
+}
 // Do not edit this line;
 module.exports = isPalindrome;
