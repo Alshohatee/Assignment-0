5df40a03ed94a8e23b0511f7f32acc55938cda23
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 01:10:50 -0500

    slove countOfAllNumbersSmallerThanTarget

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,6 +1,12 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
-  // Insert code here;
+    // Insert code here;
+    let counter = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < target)
+            counter++
+    }
+    return counter
 }
-
-// Do not edit this line;
+console.log(countOfAllNumbersSmallerThanTarget([1, 5, 3, 2, 20, 18], 18))
+    // Do not edit this line;
 module.exports = countOfAllNumbersSmallerThanTarget;
