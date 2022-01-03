e22ca7b60ac08341ca2c97fb839fa5ac9baf0e57
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 00:51:17 -0500

    slove countOfAllBooleans

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,6 +1,14 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+    // Insert code here;
+    let countBool = 0
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === false || arr[i] === true)
+            countBool++
+    }
+    return countBool
 }
 
-// Do not edit this line;
+console.log(countOfAllBooleans(["hello", "world", 3, "nice", 99, Infinity, -Infinity, true, true, true, false]))
+    // Do not edit this line;
 module.exports = countOfAllBooleans;
