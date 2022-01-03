00791cc7750c760f6ae77dc5dc766f9855231193
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 01:19:36 -0500

    slove sumOfAllEvenNumbers

--- 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
+++ 08-sumOfAllEvenNumbers/sumOfAllEvenNumbers.js
@@ -1,6 +1,15 @@
 function sumOfAllEvenNumbers(nums) {
-  // Insert code here;
+    // Insert code here;
+    let counter = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] % 2 == 0)
+            counter++
+    }
+    return counter
 }
+// console.log(
 
+//         sumOfAllEvenNumbers([2, 4, 6, 7, 8, 9, 10])
+//     )
 // Do not edit this line;
 module.exports = sumOfAllEvenNumbers;
