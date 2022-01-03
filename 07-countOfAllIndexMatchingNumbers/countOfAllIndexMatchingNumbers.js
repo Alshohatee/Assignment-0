9101b14952788bdf0033e57bb9ae7dd64e70beef
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 01:14:04 -0500

    slove countOfAllIndexMatchingNumbers

--- 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
+++ 07-countOfAllIndexMatchingNumbers/countOfAllIndexMatchingNumbers.js
@@ -1,6 +1,13 @@
 function countOfAllIndexMatchingNumbers(nums) {
-  // Insert code here;
+    // Insert code here;    
+    let counter = 0
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === i)
+            counter++
+    }
+    return counter
 }
+console.log(countOfAllIndexMatchingNumbers([0, 1, 2, 3, 4]))
 
 // Do not edit this line;
 module.exports = countOfAllIndexMatchingNumbers;
