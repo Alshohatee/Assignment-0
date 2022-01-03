22d4376ea0bc6afc996a4c406c705411b4129744
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 02:50:47 -0500

    slove pairSum

--- 18-pairSum/pairSum.js
+++ 18-pairSum/pairSum.js
@@ -1,5 +1,20 @@
 function pairSum(nums, target) {
-  // Insert code here;
+    // Insert code here;
+
+    // checking if the length is 1 or less 
+    if (nums.length <= 1) {
+        deferred.reject(nums);
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < nums.length; j++) {
+            if (j !== i) {
+                if (nums[i] + nums[j] == target)
+                    return true;
+            }
+        }
+    }
+    return false;
 }
 
 // Do not edit this line;
