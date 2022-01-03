36881a7dfc568c0cbe25bd96653b0e458a5680c7
alshohatee <alshohateeaseel@gmail.com>
2022-01-03 01:02:42 -0500

    solve countOfAllBooleansAndStrings

--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,6 +1,13 @@
 function countOfAllBooleansAndStrings(arr) {
-  // Insert code here;
-}
+    // Insert code here;
+    let count = 0
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === "string" || arr[i] === false || arr[i] === true)
+            count++
 
+    }
+    return count
+}
+// console.log(countOfAllBooleansAndStrings([true, "true", false, "false", 9]))
 // Do not edit this line;
 module.exports = countOfAllBooleansAndStrings;
